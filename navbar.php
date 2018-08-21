ccc66c3ad3abd2aafb7c932de00c2f3b1a0d2fc8
George Trieu <geocrafterserver@gmail.com>
2018-08-21 00:13:29 -0400

    Fixing Hack Language

--- navbar.php
+++ navbar.php
@@ -1,9 +1,12 @@
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="./">Home</a></li>
-      <li><a href="login">Login</a></li>
-      <li><a href="register">Register</a></li>
-    </ul>
-  </div>
-</nav>
+<!DOCTYPE html>
+<html>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="./">Home</a></li>
+        <li><a href="login">Login</a></li>
+        <li><a href="register">Register</a></li>
+      </ul>
+    </div>
+  </nav>
+</html>
